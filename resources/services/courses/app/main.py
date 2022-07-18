1e19dab63e302576b0427ed36e13e8ff7390f363
Francisco Quesada <fquesada@itba.edu.ar>
2022-07-17 22:29:26 -0300

    add prefix router to services

--- resources/services/courses/app/main.py
+++ resources/services/courses/app/main.py
@@ -1,17 +1,35 @@
-#reference: https://fastapi.tiangolo.com/deployment/docker/
+# reference: https://fastapi.tiangolo.com/deployment/docker/
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import APIRouter, FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+prefix_router = APIRouter(prefix="/courses")
+
+
+class Course(BaseModel):
+    id: str
+    name: str
+    teachers: list
+    cover_image: str
+
 
 @app.get("/health-check")
 def health_check():
     return {"status": "ok"}
 
 
+@prefix_router.get("/{course_id}",)
+async def get_course(course_id: str):
+    mock_course = Course(
+        id=course_id,
+        name="Python",
+        teachers=["John", "Jane"],
+        cover_image="https://www.python.org/static/img/python-logo.png"
+    )
+    return mock_course
 
-@app.get("/courses")
-def read_root():
-    return "courses info"
 
+app.include_router(prefix_router)
--- resources/services/users/app/main.py
+++ resources/services/users/app/main.py
@@ -1,13 +1,31 @@
 # reference: https://fastapi.tiangolo.com/deployment/docker/
+import enum
+import string
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import APIRouter, FastAPI
 
 import boto3
+from pydantic import BaseModel
+
+
+class Role(enum.Enum):
+    STUDENT = "student"
+    TEACHER = "teacher"
+
+
+class User(BaseModel):
+    id: str
+    email: str
+    full_name: str
+    role: Role
+
 
 app = FastAPI()
 
 dyanamodb = boto3.client('dynamodb')
+
+prefix_router = APIRouter(prefix="/users")
 
 
 @app.get("/health-check")
@@ -15,12 +33,17 @@ def health_check():
     return {"status": "ok"}
 
 
-@app.get("/users")
-def read_root():
+@prefix_router.get("/{user_id}")
+async def get_user(user_id: str):
 
-    response = dyanamodb.get_item(TableName='users', Key={
-                                  'user_id': {'S': '1234'}})
+    mock_user = User(
+        id=user_id,
+        email="mock@email.com",
+        full_name="Mock User",
+        role=Role.STUDENT
+    )
 
-    return {
-        "data": response['Item']['user_id']['S']
-    }
+    return mock_user
+
+
+app.include_router(prefix_router)
